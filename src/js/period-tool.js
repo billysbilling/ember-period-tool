08dae8d765533b87be03265ee742d59f9da60ab4
Sebastian Seilund <sebastian@seilund.com>
2014-01-05 16:32:58 -0800

    use new hasAction property from ember-tool

--- src/js/period-tool.js
+++ src/js/period-tool.js
@@ -1,5 +1,7 @@
 module.exports = require('ember-tool').extend(require('ember-period-field').PeriodFieldMixin, {
     icon: 'icons/calendar',
 
-    text: Em.computed.alias('value.name')
+    text: Em.computed.alias('value.name'),
+    
+    hasAction: true
 });
