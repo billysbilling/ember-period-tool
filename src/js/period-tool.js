8ffef78c81f8cdf8e753c1fed8228bdf29ecd076
Michael Storgaard <github@michaelstorgaard.com>
2014-03-06 16:12:54 +0100

    Added option to show default text

--- src/js/period-tool.js
+++ src/js/period-tool.js
@@ -1,7 +1,17 @@
+var i18nContext = require('i18n-context')('ember_period_tool', require.resolve('../locales')),
+    t = i18nContext.t;
+
 module.exports = require('ember-tool').extend(require('ember-period-field').PeriodFieldMixin, {
     icon: 'icons/calendar',
 
-    text: Em.computed.alias('value.name'),
-    
-    hasAction: true
+    text: function() {
+        if (this.get('showDefaultText')) {
+            return this.get('value.name') || t('all_time');
+        }
+        return this.get('value.name');
+    }.property('value.name', 'showDefaultText'),
+
+    hasAction: true,
+
+    showDefaultText: false
 });
